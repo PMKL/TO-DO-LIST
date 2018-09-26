66c4c6cfcf0ad07247dcc839dc15ae6410a148bb
PMKL <cast0051@stthomas.edu>
2018-09-26 13:09:36 -0500

    Final Changes

--- script.js
+++ script.js
@@ -2,16 +2,20 @@
 //CISC 375
 //About Section for Assignment 1
 
+//To-Do-List Code
+
 // Open the lightBox/Modal
+//And display it
 function openModal() {
 	document.getElementById('myModal').style.display = "block";
 }
 
-// Close it
+// Close the lightbox and set diplay to none again
 function closeModal() {
 	document.getElementById('myModal').style.display = "none";
 }
 
+//Initialize the index
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -20,29 +24,36 @@ function plusSlides(n) {
 	showSlides(slideIndex += n);
 }
 
-// Thumbnail click conrol
+// Thumbnail click to image
 function currentSlide(n) {
 	showSlides(slideIndex = n);
 }
 
+//Slideshow function for moving across images
 function showSlides(n) {
 	var i;
 	var slides = document.getElementsByClassName("mySlides");
 	var dots = document.getElementsByClassName("demo");
-	var captionText = document.getElementById("caption");
+
+	//Reset if index goes beyond 20
 	if (n > slides.length) {
 		slideIndex = 1
 	}
+	//Go to the top if index is less than 1
 	if (n < 1) {
 		slideIndex = slides.length
 	}
+	//hide previous images if moved away
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
 	}
+	//slide show code, not in use
 	for (i = 0; i < dots.length; i++) {
 		dots[i].className = dots[i].className.replace(" active", "");
 	}
 	slides[slideIndex - 1].style.display = "block";
 	dots[slideIndex - 1].className += " active";
-	captionText.innerHTML = dots[slideIndex - 1].alt;
+
+	//Gallery slideshow code
+	//ToDoList code
 }
